1692163c0b66afb8c292d2f66e6e1500a380c66a
Sai Aditya <dsaiaditya@gmail.com>
2020-09-04 17:51:37 +0530

    Updates

---
+++ precache-manifest.0d477fcb1dc206ab1e315b7bd9d1770f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1dc4cc47297bd71dfeb44417843cda",
+    "url": "/TopMovieSeries/index.html"
+  },
+  {
+    "revision": "bc1f335cb4e5848cc0e7",
+    "url": "/TopMovieSeries/static/css/main.17b26c62.chunk.css"
+  },
+  {
+    "revision": "25a7cbc6c2895e19dc03",
+    "url": "/TopMovieSeries/static/js/2.b8112745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TopMovieSeries/static/js/2.b8112745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1f335cb4e5848cc0e7",
+    "url": "/TopMovieSeries/static/js/main.983d453f.chunk.js"
+  },
+  {
+    "revision": "198df64d770f43ed0f61",
+    "url": "/TopMovieSeries/static/js/runtime-main.8f3a7630.js"
+  },
+  {
+    "revision": "4c2de83fa7f10a245271dafb112e116e",
+    "url": "/TopMovieSeries/static/media/background.4c2de83f.jpg"
+  }
+]);
